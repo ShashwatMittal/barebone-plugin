e6079d88afa865b97af9cd43cb9b29acf5eed3b9
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-10-09 11:49:00 +0530

    Initial commit

---
+++ barebone-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Barebone Plugin
+Plugin URI:   https://github.com/ShashwatMittal/barebone-plugin
+Description:  This is a basic barebone plugin.
+Version:      1.0.0
+Author:       Shashwat Mittal <shashwat.mittal5@gmail.com>
+Author URI:   https://shashwatmittal.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  smbb
+Domain Path:  /languages
+*/
